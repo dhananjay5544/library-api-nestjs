503166d60d26df7d4b2e81b3f6d6e8f83a376189
Dhananjay Shinde <Dhananjay.Shinde@emtecinc.com>
2021-02-03 17:36:04 +0530

    db-migration fix

--- src/migration/1612271315082-database.ts
+++ src/migration/1612271315082-database.ts
@@ -2,14 +2,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class database1612271315082 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query('create schema if not exists `library`;');
     queryRunner.query('create schema if not exists `test-library`;');
     queryRunner.query('create schema if not exists `dev-library`;');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.query('drop schema if not exists `library`;');
-    queryRunner.query('drop schema if not exists `test-library`;');
-    queryRunner.query('drop schema if not exists `dev-library`;');
+    queryRunner.query('drop schema if exists `test-library`;');
+    queryRunner.query('drop schema if exists `dev-library`;');
   }
 }
